2e00c48eb2c64f19abe51e4668796ae29ec64dc5
Ivan Bahrii <ivan.bahrii@davita.com>
2021-12-07 09:50:05 -0800

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,27 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FirstTest {
+
+  @Test
+  public void testZero() {
+
+    assertEquals(0, 0);
+  }
+
+  @Test
+  public void testTwo() {
+
+    assertEquals(2, 2);
+  }
+
+  @Test
+  public void testThree() {
+
+    assertEquals(3, 3);
+  }
+
+}
